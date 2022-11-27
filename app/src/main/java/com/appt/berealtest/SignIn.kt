9360c46cf53162bb684d981fea97daa7356a83cb
Andy <andrew.roberts86@hotmail.co.uk>
2022-11-27 18:39:23 +0000

    Move to TextFieldValue

--- app/src/main/java/com/appt/berealtest/SignIn.kt
+++ app/src/main/java/com/appt/berealtest/SignIn.kt
@@ -5,35 +5,34 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.appt.berealtest.ui.theme.BeRealTestTheme
 
 
 @Composable
 fun SignIn(error: Boolean, signIn: (userName: String, password: String) -> Unit) {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    val username = remember { mutableStateOf(TextFieldValue()) }
+    val password = remember { mutableStateOf(TextFieldValue()) }
 
     Column {
 
 
         Text(text = stringResource(R.string.signInTitle))
         TextField(
-            value = username,
-            onValueChange = { username = it },
+            value = username.value,
+            onValueChange = { username.value = it },
             label = { Text(stringResource(R.string.username)) }
         )
         TextField(
-            value = password,
-            onValueChange = { password = it },
+            value = password.value,
+            onValueChange = { password.value = it },
             label = { Text(stringResource(R.string.password)) }
         )
-        Button(onClick = { signIn(username, password) }) {
+        Button(onClick = { signIn(username.value.text, password.value.text) }) {
             Text(text = stringResource(R.string.signIn))
         }
     }
