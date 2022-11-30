0e04649ce423c59a0565595bb30582d66db00610
Andy <andrew.roberts86@hotmail.co.uk>
2022-11-30 16:44:31 +0000

    Something.... Something... Navigation

--- app/src/main/java/com/appt/berealtest/MainActivity.kt
+++ app/src/main/java/com/appt/berealtest/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.appt.berealtest.ui.theme.BeRealTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,12 +33,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainActivityContent(viewModel: FileExplorerViewModel = viewModel(factory = FileExplorerViewModel.Factory)) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "signIn") {
+        composable("signIn/{error}") {
+            SignIn(
+                error = uiState.error,
+                signIn = { username, password -> viewModel.signIn(username, password) }
+            )
+        }
+    }
     val uiState = viewModel.uiState.value
 
     if (uiState.selectedImage != null) {
         ImageHolder(uiState.selectedImage)
     } else if (uiState.signedIn) {
-        FileExplorer(uiState.directories, uiState.images, viewModel::openDirectory, viewModel::selectImage)
+        FileExplorer(
+            uiState.directories,
+            uiState.images,
+            viewModel::openDirectory,
+            viewModel::selectImage
+        )
     } else {
         SignIn(
             error = uiState.error,
