8ad537c6b5391fd5a6ca188329cbfec886eb88db
Andy <andrew.roberts86@hotmail.co.uk>
2022-11-30 15:26:39 +0000

    Sign out on fail directory

--- app/src/androidTest/java/com/appt/berealtest/MainActivityTest.kt
+++ app/src/androidTest/java/com/appt/berealtest/MainActivityTest.kt
@@ -2,8 +2,11 @@ package com.appt.berealtest
 
 import FileDirectory
 import ImageFile
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import org.junit.Rule
 import org.junit.Test
 
@@ -49,12 +52,7 @@ class MainActivityTest {
             MainActivityContent(viewModel)
         }
 
-        composeTestRule.onNodeWithText("Username").performTextInput("someUser")
-        composeTestRule.onNodeWithText("Password").performTextInput("somePassword")
-        composeTestRule.onNodeWithText("Sign In").performClick()
-        imageService.whenSignInSucceeds(
-            FileDirectory("id-1", "someDirectory")
-        )
+        whenUserSignsIn()
 
         composeTestRule.onNodeWithText("someDirectory").performClick()
         imageService.thenGetDirectoryIsCalled(
@@ -70,32 +68,37 @@ class MainActivityTest {
             listOf(ImageFile("456", "NewImage"))
         )
 
-        composeTestRule.waitUntil {
-            composeTestRule.onAllNodesWithText("NewDirectory").fetchSemanticsNodes().size == 1
-        }
         composeTestRule.onNodeWithText("NewDirectory").assertIsDisplayed()
         composeTestRule.onNodeWithText("NewImage").assertIsDisplayed()
         composeTestRule.onNodeWithText("someDirectory").assertDoesNotExist()
     }
 
     @Test
-    fun shouldSignUseOutWhenGetDirectoryFails() {
+    fun shouldSignOutWhenGetDirectoryFails() {
         composeTestRule.setContent {
             MainActivityContent(viewModel)
         }
 
-        composeTestRule.onNodeWithText("Username").performTextInput("someUser")
-        composeTestRule.onNodeWithText("Password").performTextInput("somePassword")
-        composeTestRule.onNodeWithText("Sign In").performClick()
-        imageService.whenSignInSucceeds(
-            FileDirectory("id-1", "someDirectory")
-        )
+        whenUserSignsIn()
 
         composeTestRule.onNodeWithText("someDirectory").performClick()
         imageService.thenGetDirectoryIsCalled(
             "someUser",
             "somePassword",
             "id-1"
+        )
+
+        imageService.whenGetDirectoryFails()
+        composeTestRule.onNodeWithText("Something went wrong").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Username").assertIsDisplayed()
+    }
+
+    private fun whenUserSignsIn() {
+        composeTestRule.onNodeWithText("Username").performTextInput("someUser")
+        composeTestRule.onNodeWithText("Password").performTextInput("somePassword")
+        composeTestRule.onNodeWithText("Sign In").performClick()
+        imageService.whenSignInSucceeds(
+            FileDirectory("id-1", "someDirectory")
         )
     }
 
