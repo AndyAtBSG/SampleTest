330b0abd46cfa58c213cb0df88c3fb7c765b46fd
Andy <andrew.roberts86@hotmail.co.uk>
2022-12-01 20:23:18 +0000

    Image load refactor

--- app/src/main/java/com/appt/berealtest/imageViewer/ImageViewer.kt
+++ app/src/main/java/com/appt/berealtest/imageViewer/ImageViewer.kt
@@ -2,6 +2,7 @@ package com.appt.berealtest.imageViewer
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.appt.berealtest.BuildConfig
@@ -14,21 +15,21 @@ import com.bumptech.glide.load.model.LazyHeaders
 
 private const val getImageEndpoint = "/items/%s/data"
 
-@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun ImageViewer(imageId: String) {
-    val url = GlideUrl(
-        BuildConfig.SERVER_URL + String.format(getImageEndpoint, imageId),
-        LazyHeaders.Builder()
-            .addHeader("Authorization", "Basic $BASE64_AUTH")
-            .build()
-    )
-
     GlideImage(
         model = BuildConfig.SERVER_URL + String.format(getImageEndpoint, imageId),
         contentDescription = stringResource(R.string.contentDescriptionImage),
         modifier = Modifier.fillMaxSize()
     ) {
+        val url = GlideUrl(
+            BuildConfig.SERVER_URL + String.format(getImageEndpoint, imageId),
+            LazyHeaders.Builder()
+                .addHeader("Authorization", "Basic $BASE64_AUTH")
+                .build()
+        )
+
         it.load(url)
     }
 }
