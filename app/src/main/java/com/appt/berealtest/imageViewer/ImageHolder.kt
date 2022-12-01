9d919259164f1074e86a0924061b149dd553e8d6
Andy <andrew.roberts86@hotmail.co.uk>
2022-12-01 18:00:54 +0000

    Use real URL

--- app/src/main/java/com/appt/berealtest/imageViewer/ImageHolder.kt
+++ app/src/main/java/com/appt/berealtest/imageViewer/ImageHolder.kt
@@ -2,15 +2,18 @@ package com.appt.berealtest.imageViewer
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import com.appt.berealtest.BuildConfig
 import com.appt.berealtest.R
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+
+private const val getImageEndpoint = "/items/%s/data"
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun ImageViewer(imageId: String) {
     GlideImage(
-        stringResource(R.string.imageUrl, imageId),
+        BuildConfig.SERVER_URL + String.format(getImageEndpoint, imageId),
         stringResource(R.string.contentDescriptionImage)
     )
 }
