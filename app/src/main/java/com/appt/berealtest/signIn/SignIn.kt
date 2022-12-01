f3862d3a319ae5ea75f796abdc26118f6d25a9fc
Andy <andrew.roberts86@hotmail.co.uk>
2022-12-01 15:11:58 +0000

    Sign in styling

--- app/src/main/java/com/appt/berealtest/signIn/SignIn.kt
+++ app/src/main/java/com/appt/berealtest/signIn/SignIn.kt
@@ -1,15 +1,22 @@
 package com.appt.berealtest.signIn
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.appt.berealtest.R
 import com.appt.berealtest.ui.theme.BeRealTestTheme
@@ -38,27 +45,50 @@ fun SignInContent(
     val username = remember { mutableStateOf(TextFieldValue()) }
     val password = remember { mutableStateOf(TextFieldValue()) }
 
-    Column {
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+    ) {
 
-        Text(text = stringResource(R.string.signInTitle))
+        Text(
+            text = stringResource(R.string.signInTitle),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(0.dp, 24.dp),
+        )
 
         TextField(
             value = username.value,
             onValueChange = { username.value = it },
-            label = { Text(stringResource(R.string.username)) }
+            label = { Text(stringResource(R.string.username)) },
+            modifier = Modifier.fillMaxWidth()
         )
 
         TextField(
             value = password.value,
             onValueChange = { password.value = it },
-            label = { Text(stringResource(R.string.password)) }
+            label = { Text(stringResource(R.string.password)) },
+            modifier = Modifier.fillMaxWidth()
         )
 
-        Button(onClick = { signIn(username.value.text, password.value.text) }) {
+        if (showError) {
+            Text(
+                text = stringResource(R.string.error),
+                color = Color.Red
+            )
+        }
+
+        Button(
+            onClick = { signIn(username.value.text, password.value.text) },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(0.dp, 24.dp)
+        ) {
             Text(text = stringResource(R.string.signIn))
         }
 
-        if (showError) Text(text = stringResource(R.string.error))
     }
 }
 
